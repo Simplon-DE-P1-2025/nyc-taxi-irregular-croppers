1a0ac33a71fc0cd8b2e6b441f8cbe185afe5180c
alexandreSeverien <132767040+alexandreSeverien@users.noreply.github.com>
2026-06-04 16:50:37 +0200

    fix: ajout de round sur les colonnes avec des moyennes pour l'affichage

--- dbt_nyc_taxi/models/marts/hourly_patterns.sql
+++ dbt_nyc_taxi/models/marts/hourly_patterns.sql
@@ -9,7 +9,7 @@ SELECT
     pickup_hour,
     COUNT(*) AS trip_count,
     round(AVG(trip_distance),2) AS avg_distance,
-    AVG(total_amount) AS avg_revenue,
+    ROUND(AVG(total_amount)) AS avg_revenue,
     round(sum(trip_distance) / nullif(sum(trip_duration_minutes) / 60.0, 0), 2) as avg_speed
 FROM {{ ref('int_trip_metrics') }}
 GROUP BY 1
