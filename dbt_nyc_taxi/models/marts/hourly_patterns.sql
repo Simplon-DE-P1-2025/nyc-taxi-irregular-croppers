779aed4eb47f829a568742ddee2f6fc4482875a9
alexandreSeverien <132767040+alexandreSeverien@users.noreply.github.com>
2026-06-04 16:43:36 +0200

    Update dbt_nyc_taxi/models/marts/hourly_patterns.sql
    Co-authored-by: 5amchaka <samchaka@orange.fr>

--- dbt_nyc_taxi/models/marts/hourly_patterns.sql
+++ dbt_nyc_taxi/models/marts/hourly_patterns.sql
@@ -8,7 +8,7 @@
 SELECT
     pickup_hour,
     COUNT(*) AS trip_count,
-    AVG(trip_distance) AS avg_distance,
+    round(AVG(trip_distance),2) AS avg_distance,
     AVG(total_amount) AS avg_revenue,
     round(sum(trip_distance) / nullif(sum(trip_duration_minutes) / 60.0, 0), 2) as avg_speed
 FROM {{ ref('int_trip_metrics') }}
